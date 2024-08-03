679e2152f1e656f6b41032c9328247551ffd0272
Faiz <220701167@rajalakshmi.edu.in>
2024-08-03 22:23:51 +0530

    adding cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,7 @@ products.forEach((product) =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary data-product-id = ${product.id}">
             Add to Cart
           </button>
         </div>
@@ -54,3 +54,35 @@ products.forEach((product) =>{
 });
 
 document.querySelector('.js-product-grid').innerHTML = productsHTML
+
+
+document.querySelectorAll('.add-to-cart-button').forEach((button) => {
+        button.addEventListener('click' , () => {
+            const productId = button.dataset.productId
+            let matchingItem
+
+            cart.forEach((item) => {
+                if(productId === item.productId) matchingItem = item
+            })
+
+            if(matchingItem){
+                matchingItem.quantity += 1
+            }
+            else{
+                cart.push({
+                    productId : productId,
+                    quantity: 1
+                })
+            }
+
+            let cartQuantity =''
+
+            cart.forEach((item) => {
+                cartQuantity = item.quantity
+            }) 
+
+            
+        })
+    })
+
+
